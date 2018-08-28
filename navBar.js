c1893f6f42c90d229738b02e437331fc17a7cef5
mneira10 <30906976+mneira10@users.noreply.github.com>
2018-08-27 23:43:07 -0500

    Waiting for doc to be ready b4 executing js
    "A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you. Code included inside $( document ).ready() will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute." more info: http://learn.jquery.com/using-jquery-core/document-ready/

--- navBar.js
+++ navBar.js
@@ -1,34 +1,37 @@
-// Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            $('html, body').animate({
-                scrollTop: (target.offset().top - 56)
-            }, 1000, "easeInOutExpo");
-            return false;
+//wait for page to be ready, then make changes.
+$( document ).ready(function() {
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 56)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
         }
-    }
-});
+    });
 
-//Activate Scrollspy
-$('body').scrollspy({
-    target: '#mainNav',
-    offset: 57
-});
+    //Activate Scrollspy
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 57
+    });
 
-//Make navbar transparent
-var navbarCollapse = function () {
-    if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").removeClass("navbar-transparent");
-    } else {
-        $("#mainNav").addClass("navbar-transparent");
-    }
-};
+    //Make navbar transparent
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").removeClass("navbar-transparent");
+        } else {
+            $("#mainNav").addClass("navbar-transparent");
+        }
+    };
 
-$(window).scroll(navbarCollapse);
+    $(window).scroll(navbarCollapse);
 
-$('.js-scroll-trigger').click(function () {
-    $('.navbar-collapse').collapse('hide');
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });    
 });
